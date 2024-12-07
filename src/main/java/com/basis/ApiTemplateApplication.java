29f97dd5a546335ce663cf6c8f73985de5eb4d57
IT派同学 <108468186+pdxjie@users.noreply.github.com>
2024-12-07 21:31:52 +0800

    feat: 初始化模板

---
+++ src/main/java/com/basis/ApiTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.basis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiTemplateApplication.class, args);
+    }
+
+}
