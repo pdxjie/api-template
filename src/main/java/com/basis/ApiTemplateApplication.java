18d810713b30e4cc7b4094a95c4af33da87c09d8
IT派同学 <108468186+pdxjie@users.noreply.github.com>
2024-12-07 22:20:55 +0800

    fix: 启动类添加 mapper 扫描注解以及开启 Aspect 代理 注解

--- src/main/java/com/basis/ApiTemplateApplication.java
+++ src/main/java/com/basis/ApiTemplateApplication.java
@@ -1,9 +1,13 @@
 package com.basis;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@MapperScan("com.basis.mapper")
+@EnableAspectJAutoProxy(exposeProxy = true)
 public class ApiTemplateApplication {
 
     public static void main(String[] args) {
